9fe6a7a6b5ce093d8cc3fd8b73c21fc327cacaaa
Ramazan Alkoyun <ramazan.resan@gmail.com>
2024-11-04 13:51:42 +0100

    Refactor ExpensesOutput component to handle fallback text

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -10,11 +10,15 @@ const RecentExpenses = () => {
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
-    return (expense.date >= date7DaysAgo) && (expense.date <= today);
+    return expense.date >= date7DaysAgo && expense.date <= today;
   });
 
   return (
-    <ExpensesOutput expenses={recentExpenses} expensesPeriod="Last 7 period" />
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod="Last 7 period"
+      fallbackText="No expenses registered for the last 7 days"
+    />
   );
 };
 
